0a98e3492179ce65e27549fcd36d731d76afd799
Giovanni Cappelletti <tech@methodwolf.com>
2025-01-21 16:11:56 +0100

    add ex entity

---
+++ src/main/java/org/generation/jaita138/demo6/db/entity/Park.java
@@ -0,0 +1,17 @@
+package org.generation.jaita138.demo6.db.entity;
+
+public class Park {
+
+    // name : String : VARCHAR(64)
+    // address : String : VARCHAR(128)
+    // city : String : VARCHAR(64)
+    // country : String : CHAR(2)
+
+    // area : int : INT
+    // shop : int : INT
+    // toilet : int : INT
+
+    // sports : boolean : BOOLEAN
+
+    // deleted : boolean : BOOLEAN
+}
